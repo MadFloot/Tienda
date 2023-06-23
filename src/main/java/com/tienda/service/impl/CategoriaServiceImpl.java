04a0c6e8e635e1d6e29856fc78dc97c6bd7ffcfb
MadFloot <madflootyt@gmail.com>
2023-06-23 00:07:30 -0600

    Semana 5

---
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -0,0 +1,34 @@
+package com.tienda.service.impl;
+
+import com.tienda.dao.CategoriaDao;
+import com.tienda.domain.Categoria;
+import com.tienda.service.CategoriaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+    //La anotacion Autowired crea un unico objeto sin hacer new... y se mantiene
+    @Autowired
+    private CategoriaDao categoriaDao;
+    
+   @Override
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista=categoriaDao.findAll();
+        if (activos) { //Si quiere solo las categorias activas
+            lista.removeIf(e -> !e.isActivo());
+        }
+        
+        return lista;
+    }
+        
+    
+}
+
+    
+        
+    
+
+
